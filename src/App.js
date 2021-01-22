b47745e200d9ed4779a7232dc4d5961ef6806aff
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-01-22 22:55:25 +0100

    :seedling: Creating a list of monsters
    Update App.js to display a list monsters with map()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+constructor() {
+  super();
+
+  this.state = {
+    monsters: [
+      { name: 'Frankenstein', id: 'asr1' },
+      { name: 'Dracula', id: 'asr2' },
+      { name: 'Jason', id: 'asr3' },
+    ]
+  }
+}
+
+  render() {
+    return(
+      <div className='App'> 
+        
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
