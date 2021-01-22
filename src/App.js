f02a934bb005bede7742b2b3245dc0eb99c53866
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-01-22 23:44:31 +0100

    :lipstick: Create card-list
    Create and style card-list component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import { Component } from "react";
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 class App extends Component {
-
-constructor() {
-  super();
-  //UI State
-  this.state = {
-    monsters: []
+  constructor() {
+    super();
+    //UI State
+    this.state = {
+      monsters: [],
+    };
   }
-}
   //Lifecycle Methods
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users}));
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   //Rendering UI
   render() {
-    return(
-      <div className='App'> 
-        
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+    return (
+      <div className='App'>
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
-    )
+    );
   }
 }
 
